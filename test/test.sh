e9287d3cf165e1e0e46a2ca678432ffc9cdd44db
Stefan Schallenberg aka nafets227 <infos@nafets.de>
2017-01-13 23:19:32 +0100

    pacman-repo: Added Support for Archlinux-ARM

--- test/test.sh
+++ test/test.sh
@@ -12,6 +12,104 @@ function getDir {
 	echo $THISDIR
 }
 
+##############################################################################
+function makePacmanConf {
+	arch=${1-"x86_64"}
+	rc=0
+
+	mkdir -p \
+		$THISDIR/client.$arch/var/lib/pacman       \
+		$THISDIR/client.$arch/var/cache/pacman/pkg
+	# we are keeping the /etc/pacman.d/gnupg in order to speed up.
+	# Creating new keyrings would take a long time...
+	#	$THISDIR/client.$arch/etc/pacman.d/gnupg
+	# We are not creating it either, because if this dir does not exist
+	# it will trigger key generation below.
+
+	cat >$THISDIR/pacman.$arch.conf <<-EOF
+	[options]
+	HoldPkg      = pacman glibc
+	CheckSpace
+
+	SigLevel    = Required DatabaseOptional
+	LocalFileSigLevel = Optional
+
+	# Settings for test repositories in our architecture
+	Architecture = $arch
+
+	# Standard Values:
+	#RootDir     = /
+	#DBPath      = /var/lib/pacman/
+	#CacheDir    = /var/cache/pacman/pkg/
+	#LogFile     = /var/log/pacman.log
+	#GPGDir      = /etc/pacman.d/gnupg/
+	#HookDir     = /etc/pacman.d/hooks/
+
+	RootDir      = $THISDIR/client.$arch
+	DBPath       = $THISDIR/client.$arch/var/lib/pacman/
+	CacheDir     = $THISDIR/client.$arch/var/cache/pacman/pkg/
+	GPGDir       = $THISDIR/client.$arch/etc/pacman.d/gnupg/
+
+	[core]
+	Server = $URL/\$repo/os/\$arch
+
+	[extra]
+	Server = $URL/\$repo/os/\$arch
+
+	[community]
+	Server = $URL/\$repo/os/\$arch
+	EOF
+
+	# Add Test repository only for x86_64 since our testpackage is
+	# only available on that architecture and any other architecture
+	# would create errors when trying to load the database that will
+	# not be initialised because no upload is executed.
+	if [ $arch == "x86_64" ] ; then
+		cat >>$THISDIR/pacman.$arch.conf <<-EOF
+
+		##### This is for testing our local repository
+		[test]
+		SigLevel = Optional # do not require signed packages or DB´s
+		Server = $URL/\$repo/os/\$arch
+		EOF
+	fi
+
+	if [ ! -d $THISDIR/client.$arch/etc/pacman.d/gnupg ] ; then
+		STARTTIME=$(date +%s)
+		printf "********** InitPacman $arch start ***********\n"
+		mkdir -p $THISDIR/client.$arch/etc/pacman.d/gnupg
+		# Initialize Pacman Signature DB
+
+		pacman-key --config $THISDIR/pacman.$arch.conf --init
+		rc=$?
+		if [ $rc -ne 0 ] ; then
+			dummy=bla
+		elif [ $arch == "x86_64" ] ; then
+			pacman-key --config $THISDIR/pacman.$arch.conf --populate archlinux || return 1
+			rc=$?
+		else
+			##### Install archlinuxarm Keyring #####
+			#get final URL after many redirects and download it
+			FINALURL=$( \
+				curl -I -JL https://www.archlinux.org/packages/core/any/archlinux-keyring/download | \
+				sed -n 's/^Location: \(.*\)$/\1/p' | \
+				tail -1 |\
+				sed 's:[\n\r]::g' ) && \
+			PKGFILE=$THISDIR/client.$arch/$(basename $FINALURL) && \
+			curl -o $PKGFILE $FINALURL && \
+			pacman -U $PACMAN_OPT $PKGFILE && \
+			pacman-key --config $THISDIR/pacman.$arch.conf --populate archlinuxarm
+			rc=$?
+		fi
+		if [ $rc -eq 0 ]; then
+			printf "********** InitPacman $arch success ***********\n"
+		else
+			docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
+			printf "********** InitPacman $arch Error ***********\n"
+			return 1
+		fi
+	fi
+}
 ##### Test: Uploading a package ##############################################
 function testUpload {
 	STARTTIME=$(date +%s)
@@ -44,24 +142,45 @@ function testUpload {
 	return 0
 }
 
-##### Test: Loading Repository index #########################################
-function testIndex {
+##### Test: Loading x86 Repository index #####################################
+function testX86Index {
 	STARTTIME=$(date +%s)
-	printf "*********** testIndex start **********\n"
-	pacman -Syy $PACMAN_OPT
+	printf "*********** testX86Index start **********\n"
+	pacman -Syy $PACMAN_OPT --config $THISDIR/pacman.x86_64.conf
 	if [ $? -ne 0 ]	; then
 		docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
 		printf "Error - pacman could not load the repositories through proxy\n"
 		return 1
 	fi
-	if [ ! -f client/var/lib/pacman/sync/community.db ] ; then
+	if [ ! -f client.x86_64/var/lib/pacman/sync/community.db ] ; then
 		docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
 		printf  "Error - pacman did not write %s \n" \
-			"client/var/lib/pacman/sync/community.db"
+			"client.x86_64/var/lib/pacman/sync/community.db"
 		return 1
 	fi
 
-	printf "*********** testIndex success **********\n"
+	printf "*********** testX86Index success **********\n"
+	return 0
+}
+
+##### Test: Loading ARM Repository index #####################################
+function testArmIndex {
+	STARTTIME=$(date +%s)
+	printf "*********** testArmIndex start **********\n"
+	pacman -Syy --arch armv6h $PACMAN_OPT --config $THISDIR/pacman.armv6h.conf
+	if [ $? -ne 0 ]	; then
+		docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
+		printf "Error - pacman could not load the repositories through proxy\n"
+		return 1
+	fi
+	if [ ! -f client.armv6h/var/lib/pacman/sync/community.db ] ; then
+		docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
+		printf  "Error - pacman did not write %s \n" \
+			"client.armv6h/var/lib/pacman/sync/community.db"
+		return 1
+	fi
+
+	printf "*********** testArmIndex success **********\n"
 	return 0
 }
 
@@ -69,36 +188,38 @@ function testIndex {
 function testDownload {
 	STARTTIME=$(date +%s)
 	printf "*********** testDownload start **********\n"
-	pacman -Sw $PACMAN_OPT binutils-efi
+	pacman -Sw $PACMAN_OPT --config $THISDIR/pacman.x86_64.conf binutils-efi
 	# This package does not exist in official repos, it was just uploaded
 	if [ $? -ne 0 ]	; then
 		docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
-		printf "Error - pacman could not load package pacman\n"
+		printf "Error - pacman could not load package binutils-efi\n"
 		return 1
 	fi
-	if [ ! -f client/var/cache/pacman/pkg/$TESTPKGNAM ] ; then
+	if [ ! -f client.x86_64/var/cache/pacman/pkg/$TESTPKGNAM ] ; then
 		docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
 		printf  "Error - pacman did not write %s \n" \
-			"client/var/cache/pacman/pkg/$TESTPKGNAM"
+			"client.x86_64/var/cache/pacman/pkg/$TESTPKGNAM"
 		return 1
 	fi
 
+	printf "*********** testDownload success **********\n"
 	return 0
 }
 
-##### Test: Loading a package from public #####################################
-function testLoadPkg {
+##### Test: Loading a X86 package from public ################################
+function testLoadX86Pkg {
 	STARTTIME=$(date +%s)
-	pacman -Sw $PACMAN_OPT pacman
+	printf "*********** testLoadX86Pkg start **********\n"
+	pacman -Sw $PACMAN_OPT --config $THISDIR/pacman.x86_64.conf pacman
 	if [ $? -ne 0 ]	; then
 		docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
 		printf "Error - pacman could not load package pacman\n"
 		return 1
 	fi
-	if [ ! -f client/var/cache/pacman/pkg/pacman-*-x86_64.pkg.tar.xz ] ; then
+	if [ ! -f client.x86_64/var/cache/pacman/pkg/pacman-*-x86_64.pkg.tar.xz ] ; then
 		docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
 		printf  "Error - pacman did not write %s \n" \
-			"client/var/cache/pacman/pkg/pacman-*-x86_64.pkg.tar.xz"
+			"client.x86_64/var/cache/pacman/pkg/pacman-*-x86_64.pkg.tar.xz"
 		return 1
 	fi
 	if [ ! -z "$COMP_CACHE" ] && [ ! -f $COMP_CACHE/core/os/x86_64/pacman-*-x86_64.pkg.tar.xz ] ; then
@@ -108,10 +229,36 @@ function testLoadPkg {
 		return 1
 	fi
 
-	printf "*********** testDownload success **********\n"
+	printf "*********** testLoadX86Pkg success **********\n"
 	return 0
 }
 
+##### Test: Loading a ARM package from public ################################
+function testLoadArmPkg {
+	STARTTIME=$(date +%s)
+	printf "*********** testLoadArmPkg start **********\n"
+	pacman -Sw $PACMAN_OPT --config $THISDIR/pacman.armv6h.conf pacman
+	if [ $? -ne 0 ]	; then
+		docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
+		printf "Error - pacman could not load package pacman\n"
+		return 1
+	fi
+	if [ ! -f client.armv6h/var/cache/pacman/pkg/pacman-*-armv6h.pkg.tar.xz ] ; then
+		docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
+		printf  "Error - pacman did not write %s \n" \
+			"client.armv6h/var/cache/pacman/pkg/pacman-*-armv6h.pkg.tar.xz"
+		return 1
+	fi
+	if [ ! -z "$COMP_CACHE" ] && [ ! -f $COMP_CACHE/core/os/armv6h/pacman-*-armv6h.pkg.tar.xz ] ; then
+		docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
+		printf  "Error - missing cache file %s \n" \
+			"$COMP_CACHE/core/os/armv6h/pacman-*-armv6h.pkg.tar.xz"
+		return 1
+	fi
+
+	printf "*********** testLoadArmPkg success **********\n"
+	return 0
+}
 ##############################################################################
 function testall {
 	$DOCK_COMP up -d --force-recreate --build
@@ -166,62 +313,25 @@ function testurl {
 	if [ ! -z "$CURL_USER" ] ; then CURL_USER="-u $CURL_USER"; fi
 	
 	# Setup Pacman Config
-	test -d client || mkdir client
-	rm -rf client/etc client/var >/dev/null
-	mkdir -p client/etc/pacman.d client/var/lib/pacman client/var/cache/pacman/pkg
-	cat >client/pacman.mirrorlist <<-EOF
-	##
-	## Arch Linux repository mirrorlist
-	## for Testing
-	##
-	Server = $URL/\$repo/os/\$arch
-	EOF
-	cat >client/pacman.conf <<-EOF
-	[options]
-	HoldPkg     = pacman glibc
-	Architecture = auto
-	CheckSpace
-
-	SigLevel    = Required DatabaseOptional
-	LocalFileSigLevel = Optional
-
-	[core]
-	Include = $THISDIR/client/pacman.mirrorlist
-
-	[extra]
-	Include = $THISDIR/client/pacman.mirrorlist
-
-	[community]
-	Include = $THISDIR/client/pacman.mirrorlist
-
-	##### This is for testing our local repository
-	[test]
-	SigLevel = Optional # do not require signed packages or DB´s
-	Include = $THISDIR/client/pacman.mirrorlist
-	EOF
+	for arch in x86_64 armv6h; do
+		makePacmanConf "$arch"
+		rc=$?
+		if [ $rc -ne 0 ] ; then return $rc; fi
+	done
 
 	# Tests
 	testUpload && \
-	testIndex && \
+	testX86Index && \
+	testLoadX86Pkg && \
 	testDownload && \
-	testLoadPkg
+	testArmIndex && \
+	testLoadArmPkg
 	rc=$?
 
 	# Shutdown
 	popd >/dev/null
 
 	return $rc
-}
-
-##############################################################################
-function testperl {
-	STARTTIME=$(date +%s)
-	$DOCK_COMP up -d --no-recreate --no-build
-	docker cp $THISDIR/../upload.pl pacmanrepo_pacman-repo_1:/usr/local/bin
-	$DOCK_COMP exec pacman-repo bash -c 'kill $(pidof /usr/bin/perl)'
-	$DOCK_COMP exec pacman-repo -d /usr/local/bin/upload.pl
-	curl -i $CURL_USER --data-binary @$THISDIR/$TESTPKGNAM $URL/test/upload/
-	docker logs --since=$STARTTIME pacmanrepo_pacman-repo_1
 }
 
 ##############################################################################
@@ -236,9 +346,6 @@ export THISDIR=$(getDir)
 export DOCK_COMP="docker-compose -f $THISDIR/docker-compose.yaml -p pacman-repo"
 export TESTPKGNAM="binutils-efi-2.27-1.90-x86_64.pkg.tar.xz"
 export PACMAN_OPT="-dd --noconfirm" 
-export PACMAN_OPT="$PACMAN_OPT --root client"
-export PACMAN_OPT="$PACMAN_OPT --config client/pacman.conf"
-export PACMAN_OPT="$PACMAN_OPT --cachedir client/var/cache/pacman/pkg"
 
 # default values
 URL="http://localhost:8084"
@@ -248,8 +355,6 @@ CURL_USER=""
 perl -c $THISDIR/../upload.pl
 if [ $? -ne 0 ]; then
 	printf "Syntax Error in Perl. Aborting test.\n"
-elif [ "$1" == "--perl" ]; then
-	testperl
 elif [ "$1" == "--perllocal" ]; then
 	testperllocal
 elif [ "$1" == "--client" ]; then
